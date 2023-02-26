27b46f28bb428c2b7d7cd1b04086ab502324bfbb
Shun Wang <shuonwang@gmail.com>
2023-02-26 18:39:31 +0800

    Create i18nCheck.R

---
+++ translations/i18nCheck.R
@@ -0,0 +1,55 @@
+library(potools)
+library(cli)
+
+# Generate pot meta data from R
+rPotData <- potools::get_message_data(
+  dir = ".",
+  verbose = TRUE)
+
+rErrorCalls <- subset(rPotData, rPotData$msgid=="", select = c("file","call","line_number"))
+          
+if (nrow(rErrorCalls) > 0) {
+  
+  cli_alert_danger("{nrow(rErrorCalls)} empty gettext call(s) found")
+  cli_h2("Please refer to following to resolve them:")
+  print.data.frame(rErrorCalls, row.names = FALSE)
+  
+  }else{
+    cli_alert_success("R message check PASS")
+} 
+
+cli_h1("Check QML translations:")
+# Get QML files
+qmlFiles <- as.list(list.files(path = ".", pattern = ".qml", recursive = T))
+
+
+if (length(qmlFiles) == 0) {
+  cli_alert("QML files not found")
+}
+
+# Generate pot meta data from QML
+qmlSrcData <- data.frame()
+message("Getting QML-leve messages...")
+for (i in 1:length(qmlFiles)) {
+  filePaths <- paste0("./",qmlFiles[[i]])
+      readL <- as.data.frame(readLines(filePaths))
+  tempData  <- data.frame(Source_file = rep(filePaths), 
+                            Code_line = readLines(filePaths), 
+                          Line_number = 1:nrow(readL),
+                     Translation_call = grepl(pattern="qsTr(|Id|anslate)\\(\".*\"\\)",readL[,1])+0,
+                           Empty_call = grepl(pattern="qsTr(|Id|anslate)\\(\"*\"\\)",readL[,1])+0)
+  qmlSrcData <- rbind(qmlSrcData, tempData)
+}
+
+qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call==1, select = c(1,3))
+
+if (nrow(qmlErrorCalls) > 0) {
+  
+  cli_alert_danger("{nrow(qmlErrorCalls)} empty gettext call(s) found")
+  cli_h2("Please refer to following to resolve them:")
+  
+  print.data.frame(qmlErrorCalls, row.names = FALSE)
+  
+}else{
+  cli_alert_success("QML message check PASS")
+} 
