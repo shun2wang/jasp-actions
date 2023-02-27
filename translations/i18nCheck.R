186b72bfbfad7e908755a904c59070988ba3f99e
Shun Wang <shuonwang@gmail.com>
2023-02-27 13:18:10 +0800

    typo fix

--- translations/i18nCheck.R
+++ translations/i18nCheck.R
@@ -5,10 +5,11 @@ library(potools)
 library(cli)
 
 # Generate pot meta data from R
+cli_h1("Check R translations:")
 rPotData <- potools::get_message_data(dir = ".",verbose = TRUE)
 
 # Get wrong usage of gettext of R
-rErrorCalls <- subset(rPotData, rPotData$msgid=="", select = c("file","call","line_number"))
+rErrorCalls <- subset(rPotData, rPotData$msgid=="", select = c("file", "call", "line_number"))
           
 if (nrow(rErrorCalls) > 0) {
   # Warning in CLI
@@ -29,6 +30,7 @@ if (length(qmlFiles) == 0) {
   # Generate pot meta data from QML
   qmlSrcData <- data.frame()
   message("Getting QML-leve messages...")
+
   for (i in 1:length(qmlFiles)) {
     filePaths <- paste0("./", qmlFiles[[i]])
     readL     <- as.data.frame(readLines(filePaths, warn = FALSE))
@@ -40,15 +42,15 @@ if (length(qmlFiles) == 0) {
       Translation_call = grepl(pattern="qsTr(|Id|anslate)\\(\".*\"\\)", readL[,1])+0,
       Empty_call       = grepl(pattern="qsTr(|Id|anslate)\\(\"*\"\\)", readL[,1])+0
     )
-    
     qmlSrcData <- rbind(qmlSrcData, tempData)
-    qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call == 1, select = c(1,3))
+  }
 
-    if (nrow(qmlErrorCalls) > 0) {
-      cli_alert_danger("{nrow(qmlErrorCalls)} empty gettext call(s) found")
-      cli_h2("Please refer to following to resolve them:")
-      print.data.frame(qmlErrorCalls, row.names = FALSE)
-    } else {
-      cli_alert_success("QML message check PASS")
-    } 
-}
+qmlErrorCalls <- subset(qmlSrcData, qmlSrcData$Empty_call == 1, select = c(1,3))
+
+if (nrow(qmlErrorCalls) > 0) {
+  cli_alert_danger("{nrow(qmlErrorCalls)} empty gettext call(s) found")
+  cli_h2("Please refer to following to resolve them:")
+  print.data.frame(qmlErrorCalls, row.names = FALSE)
+} else {
+  cli_alert_success("QML message check PASS")
+} 
